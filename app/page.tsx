fdade6c67357b9100204e3b5a4e6709eb698a0a7
Jinal2502 <jilu.jr11@gmail.com>
2025-12-26 23:15:48 +0530

    Fixing bugs

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,6 @@
 import ExploreBtn from "@/components/ExploreBtn";
+import EventCard from "@/components/EventCard";
+import { events, type EventItem } from "@/lib/constants";
 
 const Page = () => {
     return (
@@ -6,6 +8,21 @@ const Page = () => {
            <h1 className="text-center">The Hub for Every Dev <br /> Event You Can't Miss</h1>
            <p className="text-center mt-5">Hackathons, Meetups, and Conferences, All in One Place</p>
            <ExploreBtn/>
+
+           <div className="mt-20 space-y-7">
+                <h3>Featured Events</h3>
+
+                <ul className="events">
+                    {events && events.length > 0 && events.map((event: EventItem) => (
+                        <li key={event.title} className="list-none">
+                            <EventCard {...event} />
+                        </li>
+                    ))}
+                </ul>
+            </div>
+
+
+
 
        </section>
     )
