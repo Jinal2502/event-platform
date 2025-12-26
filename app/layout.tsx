d1d852906a914d6848cbd52f603a153bea4fbbe9
Jinal2502 <jilu.jr11@gmail.com>
2025-12-26 22:59:31 +0530

    Home page

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,55 +2,53 @@ import type { Metadata } from "next";
 import { Schibsted_Grotesk, Martian_Mono } from "next/font/google";
 import "./globals.css";
 import LightRays from "@/components/LightRays";
+import Navbar from "@/components/Navbar";
 
 const schibstedGrotesk = Schibsted_Grotesk({
-    variable: "--font-schibsted-grotesk",
-    subsets: ["latin"],
+  variable: "--font-schibsted-grotesk",
+  subsets: ["latin"],
 });
 
 const martianMono = Martian_Mono({
-    variable: "--font-martian-mono",
-    subsets: ["latin"],
+  variable: "--font-martian-mono",
+  subsets: ["latin"],
 });
 
 export const metadata: Metadata = {
-    title: "DevEvent",
-    description: "The Hub for Every Dev Event You Mustn't Miss",
+  title: "DevEvent",
+  description: "The Hub for Every Dev Event You Mustn't Miss",
 };
 
 export default function RootLayout({
-    children,
+  children,
 }: Readonly<{
-    children: React.ReactNode;
+  children: React.ReactNode;
 }>) {
-    return (
-        <html lang="en" suppressHydrationWarning className="dark">
-            <body 
-                className={`
-                    ${schibstedGrotesk.variable} 
-                    ${martianMono.variable} 
-                    min-h-screen 
-                    antialiased 
-                    bg-[#030708] 
-                    dark:bg-[#030708] 
-                    text-white
-                `}
-            >
-                <div className="absolute inset-0 top-0 z-[-1] min-h-screen">
-                    <LightRays
-                        raysOrigin="top-center-offset"
-                        raysColor="#5dfeca"
-                        raysSpeed={1.5}
-                        lightSpread={0.9}
-                        rayLength={1.4}
-                        followMouse={true}
-                        mouseInfluence={0.02}
-                        noiseAmount={0.0}
-                        distortion={0.01}
-                    />
-                </div>
-                <main>{children}</main>
-            </body>
-        </html>
-    );
+  return (
+    <html lang="en" suppressHydrationWarning className="dark">
+      <body
+        className={`${schibstedGrotesk.variable} ${martianMono.variable} min-h-screen antialiased bg-[#030708] dark:bg-[#030708] text-white`}
+      >
+      <Navbar />
+
+          <div className="absolute inset-0 top-0 z-[-1] min-h-screen">
+              <LightRays
+                  raysOrigin="top-center-offset"
+                  raysColor="#5dfeca"
+                  raysSpeed={1.5}
+                  lightSpread={0.9}
+                  rayLength={1.4}
+                  followMouse={true}
+                  mouseInfluence={0.02}
+                  noiseAmount={0.0}
+                  distortion={0.01}
+              />
+          </div>
+
+          <main>
+            {children}
+          </main>
+      </body>
+    </html>
+  );
 }
